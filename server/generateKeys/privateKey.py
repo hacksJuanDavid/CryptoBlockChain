76e824f85e9f496245e470c78f3c64f8e53885bc
hacksJuanDavid <josuacoty000@gmail.com>
2024-11-16 21:23:20 -0500

    Update generate keys in server

--- server/generateKeys/privateKey.py
+++ server/generateKeys/privateKey.py
@@ -9,53 +9,52 @@ class PrivateKey:
     # Constructor
     def __init__(self):
         # Generate private key
-        self.private_key = rsa.generate_private_key(
+        self.private_key_server = rsa.generate_private_key(
             public_exponent=65537, key_size=2048, backend=default_backend()
         )
 
-    # Function to save private key in a file
-    def save_private_key(self, private_key):
+    # Function to save private key server in a file
+    def save_private_key_server(self, private_key_server):
         # Path folder
         directory_path = os.path.join(
             "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
-            "private_key.pem",
+            "private_key_server.pem",
         )
-
-        # Save private key
+        # Save private key in a file
         with open(directory_path, "wb") as file:
             file.write(
-                private_key.private_bytes(
+                private_key_server.private_bytes(
                     encoding=serialization.Encoding.PEM,
                     format=serialization.PrivateFormat.TraditionalOpenSSL,
                     encryption_algorithm=serialization.NoEncryption(),
                 )
             )
-        print("Private key saved in private_key.pem")
+        # Return response
+        response = "Private key saved in private_key_server.pem"
+        return response
 
-    # Function to get private key
-    def get_private_key(self):
+    # Function to get private key server from a file
+    def get_private_key_server(self):
         # Path folder
         directory_path = os.path.join(
             "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
-            "private_key.pem",
+            "private_key_server.pem",
         )
-
         # Load private key
         with open(directory_path, "rb") as file:
             private_key = serialization.load_pem_private_key(
                 file.read(), password=None, backend=default_backend()
             )
-
-        # Return private key
+        # Return private key server
         return private_key
 
     # Function for test
     def test(self):
         # Save private key
-        self.save_private_key(self.private_key)
+        self.save_private_key_server(self.private_key_server)
 
         # Get private key
-        self.get_private_key()
+        self.get_private_key_server()
 
 
 # Run test if script is executed directly
--- server/generateKeys/publicKey.py
+++ server/generateKeys/publicKey.py
@@ -9,53 +9,152 @@ class PublicKey:
     # Constructor
     def __init__(self):
         # Get the private key
-        self.private_key = PrivateKey().get_private_key()
-
+        self.private_key_server = PrivateKey().get_private_key_server()
         # Generate the public key object (not bytes yet)
-        self.public_key = self.private_key.public_key()
+        self.public_key_server = self.private_key_server.public_key()
 
-    # Function save the public key to a file
-    def save_public_key(self):
+    # Function save the public key server to a file
+    def save_public_key_server(self):
         # Path to save the public key file
         directory_path = os.path.join(
             "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
-            "public_key.pem",
+            "public_key_server.pem",
         )
-
         # Convert the public key to bytes and save it
         with open(directory_path, "wb") as file:
             file.write(
-                self.public_key.public_bytes(
+                self.public_key_server.public_bytes(
                     encoding=serialization.Encoding.PEM,
                     format=serialization.PublicFormat.SubjectPublicKeyInfo,
                 )
             )
-        print("Public key saved in public_key.pem")
+        # Return the response
+        response = "Public key saved in public_key_server.pem"
+        return response
 
-    # Function to get public key from file
-    def get_public_key(self):
+    # Function save the public key client to a file
+    def save_public_key_client(self, public_key_client):
+        # Remove any leading/trailing whitespace and ensure proper newline formatting
+        public_key_client = public_key_client.strip()
+        # Convert the public key string to bytes
+        public_key_client_bytes = public_key_client.encode("utf-8")
+        # Deserialize the public key from bytes
+        public_key_client_obj = serialization.load_pem_public_key(
+            public_key_client_bytes, backend=default_backend()
+        )
+        # Path to save the public key file
+        directory_path = os.path.join(
+            "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
+            "public_key_client.pem",
+        )
+        # Save the public key to the file
+        with open(directory_path, "wb") as file:
+            file.write(
+                public_key_client_obj.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                )
+            )
+        # Return the response
+        response = "Public key saved in public_key_client.pem"
+        return response
+
+    # Function to get public server key from file
+    def get_public_key_server(self):
         # Path to the public key file
         directory_path = os.path.join(
             "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
-            "public_key.pem",
+            "public_key_server.pem",
         )
-
         # Load the public key from the file
         with open(directory_path, "rb") as file:
-            public_key = serialization.load_pem_public_key(
+            public_key_server = serialization.load_pem_public_key(
                 file.read(), backend=default_backend()
             )
+        # Return the public key server
+        return public_key_server
 
-        # Return the public key
-        return public_key
+    # Function to get public key client from file
+    def get_public_key_client(self):
+        # Path to the public key file
+        directory_path = os.path.join(
+            "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
+            "public_key_client.pem",
+        )
+        # Load the public key from the file
+        with open(directory_path, "rb") as file:
+            public_key_client = serialization.load_pem_public_key(
+                file.read(), backend=default_backend()
+            )
+        # Return the public key client
+        return public_key_client
+
+    # Function to display the public key server
+    def display_public_key_server(self):
+        # Get the public key
+        public_key_server = self.get_public_key_server()
+        # Transform the public key object to string
+        public_key_server = public_key_server.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode("utf-8")
+        # Return the public key server
+        return public_key_server
+
+    # Function to display the public key client
+    def display_public_key_client(self):
+        # Get the public key
+        public_key_client = self.get_public_key_client()
+        # Transform the public key object to string
+        public_key_client = public_key_client.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode("utf-8")
+        # Return the public key client
+        return public_key_client
+
+    # Function to check if the public key client is valid and exists
+    def check_public_key_client(self):
+        # Path to the public key file
+        directory_path = os.path.join(
+            "/home/hacksjuanda/Desktop/ProjectsDeveloment/CryptoBlockChain/server/keys",
+            "public_key_client.pem",
+        )
+        # Check if the file exists
+        if os.path.exists(directory_path):
+            return True
+        else:
+            return False
 
     # Function for test
     def test(self):
         # Save the public key
-        self.save_public_key()
+        self.save_public_key_server()
 
         # Retrieve and print the public key
-        self.get_public_key()
+        self.get_public_key_server()
+
+        # Save the public key
+        self.save_public_key_client(
+            """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgRfFazM2rSmAEzq8nVK
+noqjWiEA2O+YiYk703DHNo6v56TcQ8TuRVm4xOk0LSCuhnBNn9b0salxTxOt5x4j
+t9PEXUlyFobmYVYSnIEHsUh5lU+7unrYknYwCjTdYi11IA1zV9IsD7v3xbfUmRcE
+CaSgKBP0piVuQ8ugDwY2dk+0vaoqFn6qRNn3ZpumW9+BZ+qBCWmK5CFN7s+aREUq
+EQ6lJPS7c9w/TGOdS9Ch3ZbUulgYs/5VfE60sVjk6z4AfFOTxtt/g6XJhvRkv0m/
+g3SQH31EF3o6YKA5pOmQftEc7SNvgZCIP+akWImEH1JsCVns0qdX2VxIAcciepf+
+hwIDAQAB
+-----END PUBLIC KEY-----"""
+        )
+
+        # Retrieve and print the public key
+        self.get_public_key_client()
+
+        # Display the public key
+        self.display_public_key_server()
+
+        # Display the public key
+        self.display_public_key_client()
 
 
 # Run test if script is executed directly
