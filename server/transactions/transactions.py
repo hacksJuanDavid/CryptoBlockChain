6862f0638a9ba29d18bc2e7808e9d1976c3e0f09
hacksJuanDavid <josuacoty000@gmail.com>
2024-11-16 21:22:00 -0500

    Update transactions in server

--- server/transactions/transactions.py
+++ server/transactions/transactions.py
@@ -4,7 +4,7 @@ import json
 # Class for manage transactions
 class Transactions:
     # List of bag actions
-    list_bag_actions = [
+    bag_actions = [
         {
             "user_id": "nID1",
             "operation_type": "buy",
@@ -43,55 +43,62 @@ class Transactions:
         {
             "user_id": "nID6",
             "operation_type": "buy",
-            "stock_name": "NFLX",
-            "quantity": 3,
-            "price": 590.00,
+            "stock_name": "AAPL",
+            "quantity": 10,
+            "price": 150.00,
         },
         {
             "user_id": "nID7",
             "operation_type": "sell",
-            "stock_name": "FB",
-            "quantity": 6,
-            "price": 350.00,
+            "stock_name": "GOOGL",
+            "quantity": 5,
+            "price": 2800.00,
         },
         {
             "user_id": "nID8",
             "operation_type": "buy",
-            "stock_name": "NVDA",
-            "quantity": 15,
-            "price": 220.00,
+            "stock_name": "AMZN",
+            "quantity": 2,
+            "price": 3300.00,
         },
         {
             "user_id": "nID9",
             "operation_type": "buy",
-            "stock_name": "DIS",
-            "quantity": 4,
-            "price": 180.00,
+            "stock_name": "TSLA",
+            "quantity": 8,
+            "price": 700.00,
         },
         {
             "user_id": "nID10",
             "operation_type": "sell",
-            "stock_name": "BABA",
-            "quantity": 7,
-            "price": 150.00,
+            "stock_name": "MSFT",
+            "quantity": 12,
+            "price": 305.00,
         },
     ]
 
     # Constructor
     def __init__(self, blockchain):
+        # Instance of blockchain
         self.blockchain = blockchain
 
     # Function to list bag actions
     def list_bag_actions(self):
-        # Print list of bag actions
-        print(json.dumps(self.list_bag_actions, sort_keys=True, indent=4))
+        return self.bag_actions
+
+    # Function to my bag actions
+    def my_bag_actions(self, user_id):
+        # Get blocks with user id
+        blocks = self.blockchain.get_blocks_with_user_id(user_id)
+        # Return blocks
+        return blocks       
 
     # Function to my transactions
     def my_transactions(self, user_id):
         # Get blocks with user id
         blocks = self.blockchain.get_blocks_with_user_id(user_id)
-        # Print blocks
-        print(json.dumps(blocks, sort_keys=True, indent=4))
+        # Return blocks
+        return blocks
 
     # Function to buy
     def buy_action(self, user_id, stock_name, quantity, price):
@@ -109,11 +116,9 @@ class Transactions:
             },
             previous_hash=self.blockchain.get_previous_hash(),
         )
-        # Print buy action
-        print("Buy action successful")
-        # Print new block
-        print(
-            json.dumps(self.blockchain.get_previous_block(), sort_keys=True, indent=4)
+        # Return buy action
+        return json.dumps(
+            self.blockchain.get_previous_block(), sort_keys=True, indent=4
         )
 
     # Function to sell
@@ -132,16 +137,10 @@ class Transactions:
             },
             previous_hash=self.blockchain.get_previous_hash(),
         )
-        # Print sell action
-        print("Sell action successful")
-        # Print new block
-        print(
-            json.dumps(self.blockchain.get_previous_block(), sort_keys=True, indent=4)
+        # Return sell action
+        return json.dumps(
+            self.blockchain.get_previous_block(), sort_keys=True, indent=4
         )
-
-    # Function to my bag actions
-    def my_bag_actions(self):
-        pass
 
     # Function to test class
     def test(self):
